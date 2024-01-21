a3928686628202244cdd9efe28b5d3a495c27392
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2024-01-21 18:11:37 -0500

    Secondary type glyph closes subpanel on click

---
+++ Assets/_Scripts/_Dialogue/GlyphSelector.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GlyphSelector : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private GameObject parentUIObject;
+    #endregion
+
+    private void Start()
+    {
+        parentUIObject = this.transform.parent.parent.gameObject;
+    }
+
+    public void OnSelect()
+    {
+        parentUIObject.SetActive(false);
+    }
+}
