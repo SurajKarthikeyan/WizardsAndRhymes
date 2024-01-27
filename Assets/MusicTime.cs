60c247d39e1af6a6eed80f800218700b06c931a0
Zane ODell <odellzan@msu.edu>
2024-01-26 22:03:46 -0500

    Suraj and I's audio setup prototype thing

---
+++ Assets/MusicTime.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTime : MonoBehaviour
+{
+    public float bpm = 135;
+    public float nextBeatTime;
+
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.Play();
+        CalculateNextBeat();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextBeatTime)
+        {
+            CalculateNextBeat();
+            Debug.Log(nextBeatTime);
+        }
+    }
+
+    public void CalculateNextBeat()
+    {
+        nextBeatTime = Time.time + (1 / (bpm / 60));
+        //Debug.Log(1 / (bpm / 60));
+    }
+}
