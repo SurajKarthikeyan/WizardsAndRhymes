e3324fcea9d0e80a99bf6c179913a961e25d2f69
Zane ODell <odellzan@msu.edu>
2024-01-30 12:06:31 -0500

    Added serialized dict

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+/// <summary>
+/// Basic class for enemies, 
+/// </summary>
+
 public class Enemy : MonoBehaviour
 {
 
