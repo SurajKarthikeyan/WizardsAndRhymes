8c412b7b039d1f2483d8468ffa564eb525b9f220
Zane ODell <odellzan@msu.edu>
2024-01-31 23:54:16 -0500

    Made enemies prefabs, imported code from other project

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -1,3 +1,4 @@
+using AYellowpaper.SerializedCollections;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,25 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public List<BaseEnemy> enemies;
+
+    /// <summary>
+    /// Dictionary that holds the names of the plant prefabs and a list of strings used for their text
+    /// </summary>
+    [Tooltip("Dictionary that contains a mapping of prefab names and dialogue text")]
+    [SerializedDictionary("Plant Prefab Name", "Informative Text")]
+    public SerializedDictionary<string, List<string>> plantTextPairs;
+
+    /// <summary>
+    /// Dialogue holder reference
+    /// </summary>
+    [Tooltip("Reference to the DialogueHolder")]
+    public DialogueHolder dialogueHolder;
+
+    /// <summary>
+    /// Dialogue held within the dialogue holder
+    /// </summary>
+    private Dialogue dialogue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +40,32 @@ public class EnemyManager : MonoBehaviour
             enemy.m_Activated = true;
         }
         
+    }
+
+    /// <summary>
+    /// Updates the dialogue from the dictionary
+    /// </summary>
+    /// <param name="plantName">Name of the plant prefab</param>
+    public void UpdatePlantDialogue(string plantName)
+    {
+        //If the dictionary contains the name of the plant, we copy the text from the dictionary into the dialogue
+        if (plantTextPairs.ContainsKey(plantName))
+        {
+            List<string> dialogueStrings = plantTextPairs[plantName];
+            dialogue.dialogueSentences = new DialogueSentence[dialogueStrings.Count];
+
+            for (int i = 0; i < dialogue.dialogueSentences.Length; i++)
+            {
+                DialogueSentence currentDialogueSentence = new()
+                {
+                    sentence = dialogueStrings[i]
+                };
+                dialogue.dialogueSentences[i] = currentDialogueSentence;
+            }
+        }
+        else
+        {
+            dialogueHolder.dialogueToPlayBack.Clear();
+        }
     }
 }
