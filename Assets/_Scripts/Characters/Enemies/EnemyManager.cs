08617465edcf8e8ba33040166dc24ebe7f206719
mcdon546 <48959058+2walker2@users.noreply.github.com>
2024-02-15 16:22:50 -0500

    Enemy spawning implemented in prototype scene

--- Assets/_Scripts/Characters/Enemies/EnemyManager.cs
+++ Assets/_Scripts/Characters/Enemies/EnemyManager.cs
@@ -65,28 +65,10 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] EnemyWave[] waves = new EnemyWave[] { new EnemyWave() };
 
     [Tooltip("The index of the next wave of enemies")]
-    [SerializeField] int waveIndex = 0;
+    int waveIndex = 0;
     [Tooltip("The number of enemies, spawned and yet to be spawned, remaining in the current waves")]
-    [SerializeField] int enemiesRemaining = 0;
+    int enemiesRemaining = 0;
     #endregion
-
-    ///// <summary>
-    ///// Dictionary that holds the names of the plant prefabs and a list of strings used for their text
-    ///// </summary>
-    //[Tooltip("Dictionary that contains a mapping of prefab names and dialogue text")]
-    //[SerializedDictionary("Plant Prefab Name", "Informative Text")]
-    //public SerializedDictionary<string, List<string>> plantTextPairs;
-
-    ///// <summary>
-    ///// Dialogue holder reference
-    ///// </summary>
-    //[Tooltip("Reference to the DialogueHolder")]
-    //public DialogueHolder dialogueHolder;
-
-    ///// <summary>
-    ///// Dialogue held within the dialogue holder
-    ///// </summary>
-    //private Dialogue dialogue;
 
     #region Unity Methods
     /// <summary>
@@ -103,9 +85,6 @@ public class EnemyManager : MonoBehaviour
             Debug.LogError("You have two EnemyManagers in Scene, remove one");
             Destroy(this);
         }
-
-        //DEBUG Activate spawning immediately
-        ActivateEnemies();
     }
 
     /// <summary>
@@ -215,32 +194,5 @@ public class EnemyManager : MonoBehaviour
                 StartCoroutine(TriggerWave(nextWave));
         }
     }
-
-    /// <summary>
-    /// THIS IS TEMPORARILY HERE UNTIL I FIND SOMEWHERE TO PUT IT - Zane
-    /// </summary>
-    /// <param name="plantName">Name of the plant prefab</param>
-    //private void UpdatePlantDialogue(string plantName)
-    //{
-    //    //If the dictionary contains the name of the plant, we copy the text from the dictionary into the dialogue
-    //    if (plantTextPairs.ContainsKey(plantName))
-    //    {
-    //        List<string> dialogueStrings = plantTextPairs[plantName];
-    //        dialogue.dialogueSentences = new DialogueSentence[dialogueStrings.Count];
-
-    //        for (int i = 0; i < dialogue.dialogueSentences.Length; i++)
-    //        {
-    //            DialogueSentence currentDialogueSentence = new()
-    //            {
-    //                sentence = dialogueStrings[i]
-    //            };
-    //            dialogue.dialogueSentences[i] = currentDialogueSentence;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        dialogueHolder.dialogueToPlayBack.Clear();
-    //    }
-    //}
     #endregion
 }
