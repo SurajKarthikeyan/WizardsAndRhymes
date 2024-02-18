1dd8de397340c8d7aa19211a5b6dbe1400167ce3
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2024-02-18 13:54:49 -0500

    Inventory now also has showcase of mixtapes(seperate from ingame UI)

--- Assets/_Scripts/UI/InventoryMenu.cs
+++ Assets/_Scripts/UI/InventoryMenu.cs
@@ -3,14 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class to handle the inventory menu opening and closing(along other changes in UI when that occurs)
+/// </summary>
 public class InventoryMenu : MonoBehaviour
 {
+
+    #region Variables
+    [Tooltip("The mixtape that the player sees when outside of all menus(bottom left)")]
+    [SerializeField] private GameObject onScreenMixtapeInventory;
+    #endregion
+
+
+    #region CustomMethods
     /// <summary>
     /// Called when object is enabled, sets time scale to 0 to pause game
     /// </summary>
     private void OnEnable()
     {
         Time.timeScale = 0f;
+        onScreenMixtapeInventory.SetActive(false);
     }
 
     /// <summary>
@@ -19,5 +31,8 @@ public class InventoryMenu : MonoBehaviour
     private void OnDisable()
     {
         Time.timeScale = 1f;
+        onScreenMixtapeInventory.SetActive(true);
+
     }
+    #endregion
 }
