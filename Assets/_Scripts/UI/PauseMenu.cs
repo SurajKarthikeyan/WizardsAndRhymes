6095afd2518cfefb6f1c29347698881ea3b92029
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2024-02-18 12:58:55 -0500

    fixed code to standard

--- Assets/_Scripts/UI/PauseMenu.cs
+++ Assets/_Scripts/UI/PauseMenu.cs
@@ -11,7 +11,7 @@ using UnityEngine.UI;
 public class PauseMenu : MonoBehaviour
 {
     #region Variables
-
+    [Tooltip("Reference to the pause menu(currently unused)")]
     [SerializeField] private GameObject pauseMenu;
 
     #endregion
@@ -19,11 +19,17 @@ public class PauseMenu : MonoBehaviour
 
     #region UnityMethods
 
+    /// <summary>
+    /// Called when object is enabled, sets time scale to 0 to pause game
+    /// </summary>
     private void OnEnable()
     {
         Time.timeScale = 0f;
     }
 
+    /// <summary>
+    /// Called when object is disabled, resets time scale to 1 to continue game
+    /// </summary>
     private void OnDisable()
     {
         Time.timeScale = 1f;
@@ -33,11 +39,18 @@ public class PauseMenu : MonoBehaviour
     #endregion
 
     #region CustomMethods
+    
+    /// <summary>
+    /// Calls player controller to unpause game when resume is pressed
+    /// </summary>
     public void Resume()
     {
         PlayerController.instance.Pause();
     }
 
+    /// <summary>
+    /// Function to quit game when quit button is pressed
+    /// </summary>
     public void Quit()
     {
         Application.Quit();
