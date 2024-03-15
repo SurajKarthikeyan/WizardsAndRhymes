93074bac52036f67ee9253e596334760a3529427
mcdon546 <48959058+2walker2@users.noreply.github.com>
2024-03-15 01:39:33 -0400

    Main menu level buttons work

--- Assets/_Scripts/UI/LevelSwitcher.cs
+++ Assets/_Scripts/UI/LevelSwitcher.cs
@@ -2,21 +2,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Loads a new scene
+/// </summary>
 public class LevelSwitcher : MonoBehaviour
 {
-    public Button button1;
-    public Button button2;
-    public Button button3;
-
-    void Start()
-    {
-        // Add onClick listeners to the buttons
-        button1.onClick.AddListener(() => LoadLevel("AlphaScene"));
-        button2.onClick.AddListener(() => LoadLevel("DrewAlphaCopy"));
-        button3.onClick.AddListener(() => LoadLevel("ConnorAlphaCopy"));
-    }
-
-    void LoadLevel(string levelName)
+    /// <summary>
+    /// Load a scene by name
+    /// </summary>
+    /// <param name="levelName">The name of the scene to load</param>
+    public void LoadLevel(string levelName)
     {
         if (!string.IsNullOrEmpty(levelName))
         {
@@ -26,5 +21,14 @@ public class LevelSwitcher : MonoBehaviour
         {
             Debug.LogError("Level name is not set.");
         }
+    }
+
+    /// <summary>
+    /// Load a level by ID
+    /// </summary>
+    /// <param name="levelId">The ID of the level to load</param>
+    public void LoadLevel(int levelId)
+    {
+        SceneManager.LoadScene(levelId);
     }
 }
