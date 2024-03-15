dded6b854111822015065a1f5a72da7ff5fbc1e7
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2024-03-15 01:09:23 -0400

    Generator on off go brr

---
+++ Assets/_Scripts/_Puzzle/Generator.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+   #region Variables
+   [SerializeField] private GameObject gate;
+   [SerializeField] public bool isOn;
+   [SerializeField] private Material generatorOnMaterial;
+   [SerializeField] private Renderer generatorRenderer;
+   [SerializeField] private List<GameLight> allLights;
+   #endregion
+
+
+   #region UnityMethods
+
+   private void Start()
+   {
+      isOn = false;
+   }
+
+   private void Update()
+   {
+      for (int i = 0; i < allLights.Count(); i++)
+      {
+         if (!allLights[i].isOn)
+         {
+            return;
+         }
+      }
+      TurnOn();
+      
+   }
+
+   #endregion
+
+   #region CustomMethods
+
+   public void TurnOn()
+   {
+      isOn = true;
+      gate.SetActive(false);
+      generatorRenderer.material = generatorOnMaterial;
+      //post AK soundevent for generator ON
+   }
+
+   #endregion
+}
