571b3e13686367d4d22cf5064c2f7abc86117a7e
Zane ODell <odellzan@msu.edu>
2024-03-14 20:30:56 -0400

    code no comment but platform move

---
+++ Assets/_Scripts/_Puzzle/MovingPlatform.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public Transform startPos;
+
+    public Transform endPos;
+
+    public bool activated;
+
+    public bool movingToEnd = true;
+
+    public float movementSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated)
+        {
+            if (movingToEnd)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPos.position, movementSpeed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, endPos.position) <= 0.1f)
+                {
+                    movingToEnd = false;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPos.position, movementSpeed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startPos.position) <= 0.1f)
+                {
+                    movingToEnd = true;
+                }
+            }
+        }    
+    }
+
+    
+}
