018c628c0e9c03e26af64b3b0ee27f357e4356d7
cookand7 <113317775+cookand7@users.noreply.github.com>
2024-03-20 23:09:45 -0400

    updated main menu

--- Assets/_Scripts/UI/UITransitionManager.cs
+++ Assets/_Scripts/UI/UITransitionManager.cs
@@ -1,62 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class UITransitionManager : MonoBehaviour
 {
-    public CinemachineVirtualCamera mainMenuCamera;
-    public CinemachineVirtualCamera leftPanelCamera;
-    public CinemachineVirtualCamera rightPanelCamera;
-
-    private CinemachineVirtualCamera currentCamera;
+    public CinemachineVirtualCamera camera1;
+    public CinemachineVirtualCamera camera2;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
 
     void Start()
     {
-        // Set the initial camera
-        currentCamera = mainMenuCamera;
-        mainMenuCamera.Priority = 3; // Highest priority for main menu camera
-        leftPanelCamera.Priority = 2;
-        rightPanelCamera.Priority = 2;
+        // Set the initial state
+        camera1.Priority = 2; // Higher priority for camera1
+        camera2.Priority = 1; // Lower priority for camera2
+        button1.onClick.AddListener(OnClickButton1);
+        button4.onClick.AddListener(OnClickButton4);
+
+        // Initially, buttons 2-4 are inactive
+        button2.gameObject.SetActive(false);
+        button3.gameObject.SetActive(false);
+        button4.gameObject.SetActive(false);
     }
 
-    void Update()
+    void OnClickButton1()
     {
-        // Check mouse cursor position and update camera priority accordingly
-        UpdateCameraPriority();
+        // Switch to camera2
+        camera1.Priority = 1; // Lower priority for camera1
+        camera2.Priority = 2; // Higher priority for camera2
+
+        // Activate buttons 2-4 and deactivate button1
+        button1.gameObject.SetActive(false);
+        button2.gameObject.SetActive(true);
+        button3.gameObject.SetActive(true);
+        button4.gameObject.SetActive(true);
     }
 
-    void UpdateCameraPriority()
+    void OnClickButton4()
     {
-        Vector3 mousePosition = Input.mousePosition;
+        // Switch back to camera1
+        camera1.Priority = 2; // Higher priority for camera1
+        camera2.Priority = 1; // Lower priority for camera2
 
-        float screenWidth = Screen.width;
-        float screenHeight = Screen.height;
-
-        // Check if the mouse is on the left side of the screen
-        if (mousePosition.x < screenWidth / 3f)
-        {
-            SwitchToCamera(leftPanelCamera);
-        }
-        // Check if the mouse is on the right side of the screen
-        else if (mousePosition.x > (2f * screenWidth) / 3f)
-        {
-            SwitchToCamera(rightPanelCamera);
-        }
-        // If not on the left or right, switch to main menu camera
-        else
-        {
-            SwitchToCamera(mainMenuCamera);
-        }
-    }
-
-    void SwitchToCamera(CinemachineVirtualCamera targetCamera)
-    {
-        if (currentCamera != targetCamera)
-        {
-            currentCamera.Priority = 1; // Lower priority for the current camera
-            targetCamera.Priority = 3; // Higher priority for the target camera
-            currentCamera = targetCamera;
-        }
+        // Activate button1 and deactivate buttons 2-4
+        button1.gameObject.SetActive(true);
+        button2.gameObject.SetActive(false);
+        button3.gameObject.SetActive(false);
+        button4.gameObject.SetActive(false);
     }
 }
