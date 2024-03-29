ffc55e1da75c6c367af1dc9346439343c1207109
Zane ODell <odellzan@msu.edu>
2024-03-29 02:07:40 -0400

    more script refactoring

--- Assets/_Scripts/_Puzzle/CameraSwitch.cs
+++ Assets/_Scripts/_Puzzle/CameraSwitch.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 /// </summary>
 public class CameraSwitch : MonoBehaviour
 {
+    [Tooltip("Animator that moves the camera")]
+    [SerializeField]
+    private Animator camAnimator;
 
-    public Animator camAnimator;
-
-    public static bool is2D;
+    [Tooltip("Boolean that states if the camera is in a top down state or not")]
+    private static bool is2D;
     
+    /// <summary>
+    /// Function that is called whenever a collider enters this trigger
+    /// </summary>
+    /// <param name="other">Other collider that has entered this trigger</param>
     private void OnTriggerEnter(Collider other)
     {
         
@@ -21,6 +27,10 @@ public class CameraSwitch : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Function that is called whenever a collider exits this trigger
+    /// </summary>
+    /// <param name="other">Other collider that has exited this trigger</param>
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
