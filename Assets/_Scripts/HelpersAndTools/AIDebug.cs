37b69a6e03fa7f40737f2ffd5af484cd99d06d28
Zane ODell <odellzan@msu.edu>
2024-03-29 01:20:47 -0400

    Script cleanup

--- Assets/_Scripts/HelpersAndTools/AIDebug.cs
+++ Assets/_Scripts/HelpersAndTools/AIDebug.cs
@@ -15,9 +15,6 @@ public class AIDebug : MonoBehaviour
 
     [Tooltip("Enemy script reference to get the behavior from")]
     private BaseEnemyBehavior enemyBehavior;
-
-    [Tooltip("Enemy script reference to get the health from")]
-    private BaseEnemyHealth enemyHealth;
     #endregion
 
     #region Unity Methods
@@ -30,7 +27,6 @@ public class AIDebug : MonoBehaviour
         debugCanvas.worldCamera = Camera.main;
         debugText = debugCanvas.GetComponentInChildren <TextMeshProUGUI>();
         enemyBehavior = GetComponent<BaseEnemyBehavior>();
-        enemyHealth = GetComponent<BaseEnemyHealth>();
     }
 
     /// <summary>
@@ -45,9 +41,6 @@ public class AIDebug : MonoBehaviour
         else
         {
             debugText.text = enemyBehavior.behaviorState.ToString();
-            Vector3 rotation = Quaternion.LookRotation(Camera.main.transform.position, Vector3.up).eulerAngles;
-            // rotation.y = 180;
-            rotation.x = 0;
             debugCanvas.transform.forward = -debugCanvas.worldCamera.transform.forward;
         }
     }
