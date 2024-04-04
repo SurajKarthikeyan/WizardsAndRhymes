f532da97b23996e8d26343e3f57b74f3d6b19c1b
Zane ODell <odellzan@msu.edu>
2024-04-03 20:21:14 -0400

    player health UI works

--- Assets/_Scripts/Characters/_Player/Health/PlayerHealth.cs
+++ Assets/_Scripts/Characters/_Player/Health/PlayerHealth.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Class that handles all of the health and status of the player character
@@ -11,9 +15,46 @@ public class PlayerHealth : Health
     [SerializeField] private AK.Wwise.Event playerHurtSoundEffect;
 
     public PauseMenu pauseMenu;
+
+    public List<GameObject> hearts = new();
+
+    public Sprite fullHeartImage;
+
+    public Sprite emptyHeartImage;
+
+    private float currHealthPercentage;
  
     #endregion
-    
+
+    private void Update()
+    {
+        float healthPercentage = currentHP / maximumHP;
+
+        if (healthPercentage != currHealthPercentage)
+        {
+            currHealthPercentage = healthPercentage;
+            
+            int heartIndex = (int)(healthPercentage * hearts.Count);
+
+            for (int i = 0; i < hearts.Count; i++)
+            {
+                if (hearts[i].TryGetComponent(out Image heartImage))
+                {
+                    if (i < heartIndex)
+                    {
+                        heartImage.sprite = fullHeartImage;
+                    }
+                    else
+                    {
+                        heartImage.sprite = emptyHeartImage;
+                    }
+                }
+            
+            }
+        }
+
+    }
+
     #region Custom Methods
     /// <summary>
     /// Overridden function of all deaths
