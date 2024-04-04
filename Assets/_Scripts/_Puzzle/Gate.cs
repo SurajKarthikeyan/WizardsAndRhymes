f39b3c88084c246394e2590a9ef24b04bbcbc6bd
mcdon546 <48959058+2walker2@users.noreply.github.com>
2024-04-04 07:33:55 -0400

    Gate collider fix in lightning switch puzzle

--- Assets/_Scripts/_Puzzle/Gate.cs
+++ Assets/_Scripts/_Puzzle/Gate.cs
@@ -5,17 +5,9 @@ using UnityEngine;
 public class Gate : MonoBehaviour
 {
     [SerializeField] private Animator gateAnim;
-    [SerializeField] private BoxCollider gateCollider;
+
     public void SwitchGate()
     {
         gateAnim.SetTrigger("interaction");
-        if (gateCollider.enabled == false)
-        {
-            gateCollider.enabled = true;
-        }
-        else
-        {
-            gateCollider.enabled = false;
-        }
     }
 }
