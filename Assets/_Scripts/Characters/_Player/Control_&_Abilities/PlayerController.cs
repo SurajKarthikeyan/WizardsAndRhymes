c9b234b664ca9df9d4fffe8268392d2fea13a564
Zane ODell <odellzan@msu.edu>
2024-04-19 00:27:12 -0400

    push

--- Assets/_Scripts/Characters/_Player/Control_&_Abilities/PlayerController.cs
+++ Assets/_Scripts/Characters/_Player/Control_&_Abilities/PlayerController.cs
@@ -517,7 +517,7 @@ public class PlayerController : Singleton<PlayerController>
     /// <param name="obj">Input callback context for the ranged attack</param>
     private void DoRanged(InputAction.CallbackContext obj)
     {
-        if (canAttack)
+        if (canAttack && attackDelayCoroutine == null)
         {
             SetCanAttack(false);
             comboActive = true;
@@ -533,7 +533,7 @@ public class PlayerController : Singleton<PlayerController>
     /// <param name="obj">Input callback context for the melee attack</param>
     private void DoMelee(InputAction.CallbackContext obj)
     {
-        if (canAttack)
+        if (canAttack && attackDelayCoroutine == null)
         {
             SetCanAttack(false);
             successiveAttacks++;
