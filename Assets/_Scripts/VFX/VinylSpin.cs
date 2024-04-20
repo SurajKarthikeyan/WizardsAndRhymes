a5c141f31efef4c23e5817e7d4511803309134e3
Suraj Karthikeyan <surajkarthikeyan@gmail.com>
2024-04-20 01:57:11 -0400

    Disc in main menu with rotation and lighting change when clicked and sfx

--- Assets/_Scripts/VFX/VinylSpin.cs
+++ Assets/_Scripts/VFX/VinylSpin.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Dreamteck.Splines.Primitives;
+using Language.Lua;
 using UnityEngine;
 
 public class VinylSpin : MonoBehaviour
@@ -11,7 +12,13 @@ public class VinylSpin : MonoBehaviour
     [SerializeField] private Floater discFloater;
     [SerializeField] private int maxDegreesPerSecond;
     [SerializeField] private float timeToSpinAtMaxSpeed;
+    [SerializeField] private float timeToReachMaxSpeed;
     [HideInInspector] private float minDegreesPerSecond;
+    [SerializeField] private AK.Wwise.Event recordScratch;
+    [SerializeField] private Material pressedMat;
+    [SerializeField] private Material normalMat;
+    [SerializeField] private Renderer discRenderer;
+    private IEnumerator spinDiscInstance;
 
     #endregion
 
@@ -28,14 +35,39 @@ public class VinylSpin : MonoBehaviour
     
     public void SpinDisc()
     {
-        StartCoroutine(SpinDiscCoroutine());
+        if (spinDiscInstance == null)
+        {
+            spinDiscInstance = SpinDiscCoroutine();
+            recordScratch.Post(this.gameObject);
+            StartCoroutine(spinDiscInstance);
+        }
     }
 
     public IEnumerator SpinDiscCoroutine()
     {
-        discFloater.degreesPerSecond = maxDegreesPerSecond;
+        discRenderer.material = pressedMat;
+        float timer = 0f;
+        float newSpeed = minDegreesPerSecond;
+        while (timer < timeToReachMaxSpeed)
+        {
+            newSpeed = Mathf.Lerp(newSpeed, maxDegreesPerSecond, timer / timeToReachMaxSpeed);
+            discFloater.degreesPerSecond = newSpeed;
+            timer += Time.deltaTime;
+        }
+        
+        
         yield return new WaitForSeconds(timeToSpinAtMaxSpeed);
-        discFloater.degreesPerSecond = minDegreesPerSecond;
+        
+        timer = 0f;
+        newSpeed = maxDegreesPerSecond;
+        while (timer < timeToReachMaxSpeed)
+        {
+            newSpeed = Mathf.Lerp(newSpeed, minDegreesPerSecond, timer / timeToReachMaxSpeed);
+            discFloater.degreesPerSecond = newSpeed;
+            timer += Time.deltaTime;
+        }
+        discRenderer.material = normalMat;
+        spinDiscInstance = null;
     }
 
     #endregion
